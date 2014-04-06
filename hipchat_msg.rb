d51b94b5e4535a64ec931f67a18bce7bfc2f5af4
Eric Slick <github@slickfamily.net>
2014-04-06 01:47:56 -0700

    stylistic tweaks

--- hipchat_msg.rb
+++ hipchat_msg.rb
@@ -2,13 +2,12 @@ require 'hipchat'
 require 'singleton'
 
 class HipchatMsg
-
   include Singleton
   attr_accessor :options, :hipchat_client
 
-  instance.options = {user: ENV['HIPCHAT_USER'] || '<user>',
-                      rooms: [ENV['HIPCHAT_ROOM'] || '<room>'],
-                      token: ENV['HIPCHAT_TOKEN'] || '<hipchat_token'
+  instance.options = { user: ENV['HIPCHAT_USER'] || '<user>',
+                       rooms: [ENV['HIPCHAT_ROOM'] || '<room>'],
+                       token: ENV['HIPCHAT_TOKEN'] || '<hipchat_token'
   }
   instance.hipchat_client ||= HipChat::Client.new(instance.options[:token])
 
@@ -19,12 +18,11 @@ class HipchatMsg
   def msg(message, color)
     @options[:rooms].each do |room|
       begin
-        hipchat_client[room].send(options[:user], message, {color: color})
+        hipchat_client[room].send(options[:user], message, { color: color })
       rescue => e
         puts e.message
         puts e.backtrace
       end
     end
   end
-
 end
